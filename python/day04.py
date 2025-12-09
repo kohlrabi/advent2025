a984c0c503cb8c498ae59477fb88dab6d971974e
Jan Christoph Terasa <christoph@kohlio.de>
2025-12-09 20:11:57 +0100

    fix typehints in day04

--- python/day04.py
+++ python/day04.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
-from collections.abc import Iterable
-
 import numpy as np
 import scipy.signal
+from numpy.typing import NDArray
 
 from puzzle_input_getter import get_puzzle_input
 
 
-def part1(grid: np.ndarray) -> int:
-    kernel = np.ones((3, 3), dtype="uint")
+def part1(grid: NDArray[np.bool]) -> int:
+    kernel = np.ones((3, 3), dtype=np.uint)
     kernel[1, 1] = 0
 
     conv = scipy.signal.convolve2d(grid, kernel, mode="same")
@@ -19,8 +18,8 @@ def part1(grid: np.ndarray) -> int:
     return rolls.sum()
 
 
-def part2(lines: Iterable[str]) -> int:
-    kernel = np.ones((3, 3), dtype="uint")
+def part2(grid: NDArray[np.bool]) -> int:
+    kernel = np.ones((3, 3), dtype=np.uint)
     kernel[1, 1] = 0
 
     total = 0
@@ -40,7 +39,7 @@ def part2(lines: Iterable[str]) -> int:
 if __name__ == "__main__":
     lines = get_puzzle_input(2025, 4).splitlines()
 
-    grid = np.array([[False if x == "." else True for x in line] for line in lines], dtype=bool)
+    grid = np.array([[False if x == "." else True for x in line] for line in lines], dtype=np.bool)
 
     print(f"part1: {part1(grid)}")
     print(f"part2: {part2(grid)}")
