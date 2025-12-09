ede7b393e2dae7284ee771385253779283f77d57
Jan Christoph Terasa <christoph@kohlio.de>
2025-12-09 20:06:35 +0100

    solve day04 pt2

--- python/day04.py
+++ python/day04.py
@@ -8,23 +8,39 @@ import scipy.signal
 from puzzle_input_getter import get_puzzle_input
 
 
-def part1(lines) -> int:
+def part1(grid: np.ndarray) -> int:
     kernel = np.ones((3, 3), dtype="uint")
     kernel[1, 1] = 0
 
-    conv = scipy.signal.convolve2d(lines, kernel, mode="same")
+    conv = scipy.signal.convolve2d(grid, kernel, mode="same")
 
-    return (conv[lines] < 4).sum()
+    rolls = np.where(conv < 4, grid, False)
+
+    return rolls.sum()
 
 
 def part2(lines: Iterable[str]) -> int:
-    return 0
+    kernel = np.ones((3, 3), dtype="uint")
+    kernel[1, 1] = 0
+
+    total = 0
+    while True:
+        conv = scipy.signal.convolve2d(grid, kernel, mode="same")
+        rolls = np.where(conv < 4, grid, False)
+
+        if rolls.sum() == 0:
+            break
+
+        total += rolls.sum()
+        grid[rolls] = False
+
+    return total
 
 
 if __name__ == "__main__":
     lines = get_puzzle_input(2025, 4).splitlines()
 
-    lines = np.array([[False if x == "." else True for x in line] for line in lines], dtype=bool)
+    grid = np.array([[False if x == "." else True for x in line] for line in lines], dtype=bool)
 
-    print(f"part1: {part1(lines)}")
-    print(f"part2: {part2(lines)}")
+    print(f"part1: {part1(grid)}")
+    print(f"part2: {part2(grid)}")
